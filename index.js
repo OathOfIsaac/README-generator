1c08f1f5b8ec55dfb5fd5f809e1d449099b3946c
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-04-09 13:49:00 -0500

    created inquierer prompt with one question in array

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // TODO: Include packages needed for this application
+    const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"username",
+        message:"What is your GitHub username?"            
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(
+        questions
+    ).then(function(data){
+        console.log(data)
+    })
 }
 
 // Function call to initialize app
