ee472f2192add9c284763dccc86cd02468a691d3
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-04-09 14:07:03 -0500

    generate README through write to file function and imported + utilized generateMarkdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
     const inquirer = require("inquirer")
+    const fs = require("fs")
+    const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -11,7 +13,13 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data)
+    fs.writeFile( fileName, markdown, function(err){
+        if(err) throw err
+        console.log("README written")
+    } )
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -19,6 +27,15 @@ function init() {
         questions
     ).then(function(data){
         console.log(data)
+        // const fileName = "README.md"
+
+        // const markdown = generateMarkdown(data)
+        // fs.writeFile( fileName, markdown, function(err){
+        //     if(err) throw err
+        //     console.log("README written")
+        // } )
+
+        writeToFile("README.md", data)
     })
 }
 
